e659369da38a411f658fcadb85157fce895c8676
Gabriel Torres <gabrielst328@gmail.com>
2025-07-07 18:57:15 -0300

    start

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_chat/theme.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const Placeholder(),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final darkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 4, 3, 71),
+);
+
+final theme = ThemeData().copyWith(colorScheme: darkColorScheme);
