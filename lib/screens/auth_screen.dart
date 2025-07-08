b5c77356fa94c17f18e986b537f2aa680c6101ab
Gabriel Torres <gabrielst328@gmail.com>
2025-07-08 13:15:31 -0300

    feat: added validator to form inputs

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -8,80 +8,101 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
   bool _isLogin = true;
+  String _enteredEmail = '';
+  String _enteredPassword = '';
+
+  void _submit() {
+    final bool isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+    }
+    print(_enteredEmail);
+    print(_enteredPassword);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Image.asset(
+            'assets/images/chat.png',
+            fit: BoxFit.contain,
+          ),
+        ),
         title: Text('MyChat'),
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 20,
-                  top: 30,
-                  right: 20,
-                  bottom: 20,
-                ),
-                width: 200,
-                child: Image.asset('assets/images/chat.png'),
-              ),
-              Card(
-                margin: const EdgeInsets.all(20),
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Form(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextFormField(
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                            decoration: InputDecoration(
-                              label: Text('E-mail'),
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                            autocorrect: false,
-                            textCapitalization: TextCapitalization.none,
-                          ),
-                          TextFormField(
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                            decoration: InputDecoration(
-                              label: Text('Password'),
-                            ),
-                            obscureText: true,
-                          ),
-                          const SizedBox(height: 16),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text(_isLogin ? 'Entrar' : 'Cadastrar'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _isLogin = !_isLogin;
-                              });
-                            },
-                            child: Text(
-                              _isLogin ? 'Criar conta' : 'Já possuo uma conta',
-                            ),
-                          ),
-                        ],
+          child: Card(
+            margin: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _form,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        decoration: InputDecoration(
+                          label: Text('E-mail'),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.none,
+                        validator: (value) {
+                          if (value == null ||
+                              value.trim().isEmpty ||
+                              !value.contains('@')) {
+                            return 'Insira um e-mail válido';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) => _enteredEmail = newValue!,
                       ),
-                    ),
+                      TextFormField(
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        decoration: InputDecoration(
+                          label: Text('Password'),
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.trim().length < 6) {
+                            return 'A senha deve ter pelo menos 6 caracteres';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) => _enteredPassword = newValue!,
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: _submit,
+                        child: Text(_isLogin ? 'Entrar' : 'Cadastrar'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        },
+                        child: Text(
+                          _isLogin ? 'Criar conta' : 'Já possuo uma conta',
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
